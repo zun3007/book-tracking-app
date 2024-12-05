cae2e942f334ab915fb45cbbd6b072048db9a04a
zun3007 <141807479+zun3007@users.noreply.github.com>
2024-12-05 20:38:40 +0700

    update lin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { imageOptimizer } from './vite-image-optimizer';
 
 export default defineConfig({
+  base: '/book-tracking-app/',
   plugins: [react(), imageOptimizer()],
   build: {
     rollupOptions: {
