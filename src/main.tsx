e81d84b485de7fd51fa99263727ca293282fa5d5
zun3007 <141807479+zun3007@users.noreply.github.com>
2024-12-11 18:02:00 +0700

    update Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>
 );
